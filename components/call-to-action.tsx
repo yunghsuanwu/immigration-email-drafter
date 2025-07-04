19ed16a57351a4f332ba040d4ce34aeed345d736
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-04 18:52:12 +0100

    Modify email drafter component and call-to-action

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -1,9 +1,22 @@
 export function CallToAction() {
   return (
     <section id="call-to-action" className="py-6 md:py-10 text-center px-4 mb-8">
-      <h2 className="text-3xl md:text-5xl font-bold mb-2">Contact Your MP</h2>
+      <h2 className="text-3xl md:text-5xl font-bold mb-2">Re:Immigration</h2>
       <p className="text-muted-foreground max-w-2xl mx-auto mt-4">
-        With the immigration rule change imminent, this tool helps you draft a personalized, professional email to your Member of Parliament about your concerns regarding how the rule change will impact you.
+        With the immigration rule change imminent (
+          <a
+           href="https://notastranger.org/contactyourmp/about"
+           target="_blank"
+           rel="noopener noreferrer"
+           className="underline"
+          >
+           read what's changing 
+          </a>
+          ), we are mobilizing those who will be affected and those who know their connections will be 
+          affected to take a simple yet signicant action: Write to your Member of Parliament.
+      </p>
+      <p className="text-muted-foreground max-w-2xl mx-auto mt-4">
+        Your MP will be voting 
       </p>
     </section>
   )
--- components/project-intro.tsx
+++ components/project-intro.tsx
@@ -1,9 +1,25 @@
 export function RuleChange() {
   return (
-    <section id="project-intro" className="py-6 md:py-10 text-center px-4 mb-8">
-      <p className="font-bold text-muted-foreground max-w-2xl mx-auto mt-4">
-        Explain the imminent rule change here. Explain the white paper, too.
-      </p>
+    <section id="project-intro" className="py-6 md:py-10 flex justify-center px-4 mb-8">
+      <div className="max-w-2xl w-full text-left mx-auto bg-white rounded-lg shadow p-6">
+        <p className="font-bold text-muted-foreground mb-4">
+          The four changes that will take effect by 22 July if the parliament passes it:
+        </p>
+        <ul className="list-disc list-inside space-y-2">
+          <li>
+            raising the skills threshold for Skilled Worker visas, removing 111 eligible occupations;
+          </li>
+          <li>
+            closing the social care worker visa route to overseas recruitment;
+          </li>
+          <li>
+            only allowing time-limited access below degree level through a targeted immigration salary list and temporary shortage list, for critical roles only, with strict requirements for sectors to grow domestic skills;
+          </li>
+          <li>
+            commissioning the Migration Advisory Committee (MAC) to conduct a review of the temporary shortage list including occupations, salaries and benefits.
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
