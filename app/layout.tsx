3f97fa9f035e63415f3ce4b316dd44c5f6c1475a
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-05 21:57:07 +0100

    Update basepath from / to /re-immigration in nav-bar.tsx and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/re-immigration/favicon.ico" />
       </head>
       <body className={inter.className}>
         <NavBar />
