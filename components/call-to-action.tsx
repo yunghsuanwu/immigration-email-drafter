15091ad9affc7a8ea29063a46028272adbe5d4fb
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-05 20:58:21 +0100

    Update temporary read what's changing href tag in call-to-action.tsx

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -5,7 +5,7 @@ export function CallToAction() {
       <p className="text-muted-foreground max-w-2xl mx-auto mt-4">
         With the immigration rule change imminent (
           <a
-           href="http://localhost:3000/about"
+           href="https://www.gov.uk/government/news/major-immigration-reforms-delivered-to-restore-order-and-control"
            target="_blank"
            rel="noopener noreferrer"
            className="underline"
