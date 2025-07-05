bd137e5deacc22434f65c60891c95d3025754ba0
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-05 21:51:32 +0100

    Update next.config.ts after updating basepath

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/re-immigration",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/re-immigration",
-        permanent: true
-      }
-    ];
-  }
   /* config options here */
 };
 
