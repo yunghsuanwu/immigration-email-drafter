7425bdc7eee779194d2e8021dcb28bb6f074bc68
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-05 21:47:50 +0100

    Update next.config.ts to redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/re-immigration",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/re-immigration",
+        permanent: true
+      }
+    ];
+  }
   /* config options here */
 };
 
