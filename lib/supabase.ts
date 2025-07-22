17fa01143e1daaeed453201185acccc26f6471a9
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-22 11:50:21 +0100

    Add supabase processing region to eu, london

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,11 @@ import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    headers: { 'x-region': 'eu-west-2'}
+  }
+})
 
 export type Database = {
   public: {
