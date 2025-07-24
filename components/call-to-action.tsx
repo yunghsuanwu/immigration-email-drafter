459503b1c6d89f479a9c97ae7ad709c88b7523af
Yung-Hsuan Wu <wu8815@gmail.com>
2025-07-24 19:19:06 +0100

    Update project-intro

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -3,7 +3,7 @@ export function CallToAction() {
     <section id="call-to-action" className="py-6 md:py-10 text-center px-4 mb-8">
       <h2 className="text-3xl md:text-5xl font-bold mb-2">Re:Immigration</h2>
       <p className="text-muted-foreground max-w-2xl mx-auto mt-4">
-        With the immigration rule change imminent (
+        With the immigration rule change takes effect (
           <a
            href="https://www.gov.uk/government/news/major-immigration-reforms-delivered-to-restore-order-and-control"
            target="_blank"
--- components/project-intro.tsx
+++ components/project-intro.tsx
@@ -1,27 +1,42 @@
 export function RuleChange() {
   return (
-    <section id="project-intro" className="py-6 md:py-10 flex justify-center px-4 mb-8">
-      <div className="max-w-2xl w-full text-left mx-auto bg-white rounded-lg shadow p-6">
-        <p className="font-bold text-muted-foreground mb-4">
-          The four changes that will take effect by 22 July if the parliament passes it:
+    <section id="immigration-rule-change" className="max-w-2xl mx-auto py-8 md:py-12 px-4 mb-12 text-left">
+      <div>
+        <h1 className="text-3xl font-bold mb-10 text-center">2025 Immigration Rule Change: A Summary</h1>
+        <div className="max-w-2xl w-full text-left mx-auto bg-white rounded-lg shadow p-6">
+          <p className="font-bold text-muted-foreground mb-4">
+            On 22 July 2025, the UK government implements{" "}
+              <a
+           href="https://www.gov.uk/government/news/major-immigration-reforms-delivered-to-restore-order-and-control"
+           target="_blank"
+           rel="noopener noreferrer"
+           className="underline"
+          >
+            four major changes
+          </a>{" "}
+            to the immigration rules:
+          </p>
+          <ul className="list-disc list-outside space-y-2 mb-4 ml-4 pl-4">
+            <li>The skill threshold for Skilled Worker visas is now raised, and 111 eligible occupations are now removed;</li>
+            <li>The social care worker visa route to overseas recruitment is now closed;</li>
+            <li>
+              There is only time-limited access below degree level through a targeted immigration salary list 
+              and temporary shortage list, for critical roles only, with strict requirements for sectors to 
+              grow domestic skills;
+            </li>
+            <li>The Commission the Migration Advisory Committee (MAC) is to conduct a review of the temporary shortage list including occupations, salaries and benefits.</li>
+          </ul>
+        </div>
+        <p>
+          We're currently working with experts to provide a detailed summary on what these changes may mean for different stakeholders, including visa holders, those with indefinite leave to remain/settled status, employers, and beyond. 
         </p>
-        <ul className="list-disc list-inside space-y-2">
-          <li>
-            raising the skills threshold for Skilled Worker visas, removing 111 eligible occupations;
-          </li>
-          <li>
-            closing the social care worker visa route to overseas recruitment;
-          </li>
-          <li>
-            only allowing time-limited access below degree level through a targeted immigration salary list 
-            and temporary shortage list, for critical roles only, with strict requirements for sectors to 
-            grow domestic skills;
-          </li>
-          <li>
-            commissioning the Migration Advisory Committee (MAC) to conduct a review of the temporary shortage list including occupations, salaries and benefits.
-          </li>
-        </ul>
+        <p>
+          In the meantime, we encourage you to check out some resources that other migrant advisory bodies and migrants rights groups have already compiled:
+        </p>
       </div>
+        
+      
+
     </section>
   )
 }
